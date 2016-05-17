b3740ebe3d0199a04af6394525f9156684963e07
Mike Morano <mmorano@mikeandwan.us>
2016-05-17 10:29:56 -0400

    adding support for the other command line switches

--- src/NImgmin/ImgminOptions.cs
+++ src/NImgmin/ImgminOptions.cs
@@ -1,4 +1,4 @@
-using System.Text;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 
@@ -7,6 +7,15 @@ namespace NImgmin
     public class ImgminOptions
     {
         public string ImgminPath { get; set; }
+        public bool BeConservative { get; set; }
+        public bool BeVeryConservative { get; set; }
+        public double? ErrorThreshold { get; set; }
+        public double? ColorDensityRatio { get; set; }
+        public uint? MinUniqueColors { get; set; }
+        public uint? QualityOutMax { get; set; }
+        public uint? QualityOutMin { get; set; }
+        public uint? QualityInMin { get; set; }
+        public uint? MaxSteps { get; set; }
         
         
         public ImgminOptions()
@@ -22,7 +31,59 @@ namespace NImgmin
             psi.FileName = ImgminPath;
             psi.UseShellExecute = false;
             psi.RedirectStandardOutput = true;
-            psi.Arguments = $"{EscapeFilename(srcFile)} {EscapeFilename(destFile)}";
+            
+            var args = new List<string>();
+            
+            if(ErrorThreshold != null)
+            {
+                args.Add($"--error-threshold {ErrorThreshold}");
+            }
+            else
+            {
+                if(BeVeryConservative)
+                {
+                    args.Add("--very-conservative");
+                }
+                else if(BeConservative)
+                {
+                    args.Add("--conservative");
+                }
+            }
+            
+            if(ColorDensityRatio != null)
+            {
+                args.Add($"--color-density-ratio {ColorDensityRatio}");
+            }
+            
+            if(MinUniqueColors != null)
+            {
+                args.Add($"--min-unique-colors {MinUniqueColors}");
+            }
+            
+            if(QualityOutMax != null)
+            {
+                args.Add($"--quality-out-max {QualityOutMax}");
+            }
+            
+            if(QualityOutMin != null)
+            {
+                args.Add($"--quality-out-min {QualityOutMin}");
+            }
+            
+            if(QualityInMin != null)
+            {
+                args.Add($"--quality-in-min {QualityInMin}");
+            }
+            
+            if(MaxSteps != null)
+            {
+                args.Add($"--max-steps {MaxSteps}");
+            }
+            
+            args.Add(EscapeFilename(srcFile));
+            args.Add(EscapeFilename(destFile));
+              
+            psi.Arguments = string.Join(" ", args);
             
             return psi;
         }
--- test/NImgmin.Tests/NImgminTests.cs
+++ test/NImgmin.Tests/NImgminTests.cs
@@ -8,6 +8,36 @@ namespace NImgmin.Tests
 {
     public class NImgminTests
     {
+        const string FILEARGS = "\"in.jpg\" \"out.jpg\"";
+        
+        
+        [Fact]
+        public void VerifyDefaultArgs()
+        {
+            var opts = new ImgminOptions();
+            
+            Assert.Equal(FILEARGS, GetTestArgs(opts));
+        }
+        
+        
+        [Fact]
+        public void VerifyThresholdArgs()
+        {
+            var opts = new ImgminOptions();
+            opts.BeConservative = true;
+            
+            Assert.Equal($"--conservative {FILEARGS}", GetTestArgs(opts));
+            
+            opts.BeVeryConservative = true;
+            
+            Assert.Equal($"--very-conservative {FILEARGS}", GetTestArgs(opts));
+            
+            opts.ErrorThreshold = 0.2;
+            
+            Assert.Equal($"--error-threshold {opts.ErrorThreshold} {FILEARGS}", GetTestArgs(opts));
+        }
+        
+        
         [Fact]
         public void MinifyTest()
         {
@@ -19,6 +49,12 @@ namespace NImgmin.Tests
             Console.WriteLine($"size: {result?.StatsBefore?.Size} -> {result?.StatsAfter?.Size}");
             
             Assert.True(File.Exists("min.jpg"));
+        }
+        
+        
+        string GetTestArgs(ImgminOptions opts)
+        {
+            return opts.GetStartInfo("in.jpg", "out.jpg").Arguments;
         }
     }
 }
